0a2415a85b638b572c82ee6903d2d35e5921e0d3
Shogo Ogami <sg.tao.so@gmail.com>
2024-07-27 04:52:01 +0900

    Add:update language_selector component for narrow down language

--- src/components/language_selector.py
+++ src/components/language_selector.py
@@ -2,106 +2,35 @@
 import streamlit as st
 
 _LANGUAGES = {
-    "en": "english",
-    "zh": "chinese",
-    "de": "german",
-    "es": "spanish",
-    "ru": "russian",
-    "ko": "korean",
-    "fr": "french",
-    "ja": "japanese",
-    "pt": "portuguese",
-    "tr": "turkish",
-    "pl": "polish",
-    "ca": "catalan",
-    "nl": "dutch",
-    "ar": "arabic",
-    "sv": "swedish",
-    "it": "italian",
-    "id": "indonesian",
-    "hi": "hindi",
-    "fi": "finnish",
-    "vi": "vietnamese",
-    "he": "hebrew",
-    "uk": "ukrainian",
-    "el": "greek",
-    "ms": "malay",
-    "cs": "czech",
-    "ro": "romanian",
-    "da": "danish",
-    "hu": "hungarian",
-    "ta": "tamil",
-    "no": "norwegian",
-    "th": "thai",
-    "ur": "urdu",
-    "hr": "croatian",
-    "bg": "bulgarian",
-    "lt": "lithuanian",
-    "la": "latin",
-    "mi": "maori",
-    "ml": "malayalam",
-    "cy": "welsh",
-    "sk": "slovak",
-    "te": "telugu",
-    "fa": "persian",
-    "lv": "latvian",
-    "bn": "bengali",
-    "sr": "serbian",
-    "az": "azerbaijani",
-    "sl": "slovenian",
-    "kn": "kannada",
-    "et": "estonian",
-    "mk": "macedonian",
-    "br": "breton",
-    "eu": "basque",
-    "is": "icelandic",
-    "hy": "armenian",
-    "ne": "nepali",
-    "mn": "mongolian",
-    "bs": "bosnian",
-    "kk": "kazakh",
-    "sq": "albanian",
-    "sw": "swahili",
-    "gl": "galician",
-    "mr": "marathi",
-    "pa": "punjabi",
-    "si": "sinhala",
-    "km": "khmer",
-    "sn": "shona",
-    "yo": "yoruba",
-    "so": "somali",
-    "af": "afrikaans",
-    "oc": "occitan",
-    "ka": "georgian",
-    "be": "belarusian",
-    "tg": "tajik",
-    "sd": "sindhi",
-    "gu": "gujarati",
-    "am": "amharic",
-    "yi": "yiddish",
-    "lo": "lao",
-    "uz": "uzbek",
-    "fo": "faroese",
-    "ht": "haitian creole",
-    "ps": "pashto",
-    "tk": "turkmen",
-    "nn": "nynorsk",
-    "mt": "maltese",
-    "sa": "sanskrit",
-    "lb": "luxembourgish",
-    "my": "myanmar",
-    "bo": "tibetan",
-    "tl": "tagalog",
-    "mg": "malagasy",
-    "as": "assamese",
-    "tt": "tatar",
-    "haw": "hawaiian",
-    "ln": "lingala",
-    "ha": "hausa",
-    "ba": "bashkir",
-    "jw": "javanese",
-    "su": "sundanese",
-    "yue": "cantonese",
+    "en": "english",  # 英語
+    "zh": "chinese",  # 中国語
+    "de": "german",  # ドイツ語
+    "es": "spanish",  # スペイン語
+    "ru": "russian",  # ロシア語
+    "ko": "korean",  # 韓国語
+    "fr": "french",  # フランス語
+    "ja": "japanese",  # 日本語
+    "pt": "portuguese",  # ポルトガル語
+    "tr": "turkish",  # トルコ語
+    "pl": "polish",  # ポーランド語
+    "nl": "dutch",  # オランダ語
+    "ar": "arabic",  # アラビア語
+    "sv": "swedish",  # スウェーデン語
+    "it": "italian",  # イタリア語
+    "id": "indonesian",  # インドネシア語
+    "fi": "finnish",  # フィンランド語
+    "uk": "ukrainian",  # ウクライナ語
+    "el": "greek",  # ギリシャ語
+    "cs": "czech",  # チェコ語
+    "ro": "romanian",  # ルーマニア語
+    "da": "danish",  # デンマーク語
+    "hu": "hungarian",  # ハンガリー語
+    "no": "norwegian",  # ノルウェー語
+    "bg": "bulgarian",  # ブルガリア語
+    "lt": "lithuanian",  # リトアニア語
+    "lv": "latvian",  # ラトビア語
+    "sl": "slovenian",  # スロベニア語
+    "et": "estonian",  # エストニア語
 }
 
 
