76f094dd89842d3ed59c8ddf7dac6401d13679e2
Shogo Ogami <sg.tao.so@gmail.com>
2024-07-28 12:57:19 +0900

    Add:update editor_voice_input page for edit transctip

--- src/pages/12_editor_voice_input.py
+++ src/pages/12_editor_voice_input.py
@@ -18,31 +18,55 @@ def record_audio_file(audio_bytes):
         return temp_audio.name
 
 
+def update_transcript():
+    st.session_state.transcript_shown = st.session_state.transcript
+
+
 def editor_voice_input():
-    st.title("Edittin Voice Input App")
+    st.title("Editing Voice Input App")
     st.markdown("Editing and correcting a voice input.")
 
-    sidebar_key_and_model()
+    # セッション状態の初期化
+    if "transcript" not in st.session_state:
+        st.session_state.transcript = ""
+    if "transcript_shown" not in st.session_state:
+        st.session_state.transcript_shown = ""
 
+    sidebar_key_and_model()
     output_language = language_selector()
     prompt = f"Specify context or spelling in {output_language.language}."
     lang = output_language.lang_code
     st.markdown(f"Transcribe Prompt: **{prompt}**")
 
-    # Microphone-Record:
+    # マイク録音
     st.subheader("Microphone-Record")
     st.session_state.wav_audio_data = st_audiorec()
 
     if st.session_state.wav_audio_data is not None:
-        # st.audio(st.session_state.wav_audio_data, format="audio/wav")
         if st.button("Start transcription."):
+            st.session_state.transcript = ""
             wav_file = record_audio_file(st.session_state.wav_audio_data)
             transcript = ""
             with open(wav_file, "rb") as wave_data:
                 transcript = transcribe_audio(wave_data, prompt, lang)
                 if transcript != "":
-                    st.write(transcript)
                     st.session_state.transcript = transcript
+                    update_transcript()
+
+        # show transcript
+        if st.session_state.transcript_shown != "":
+            shown_transcript = f"""{st.session_state.transcript_shown}"""
+            st.code(shown_transcript, language="python")
+
+        # edit transcript
+        with st.expander(label="Edit transcript", icon="🖌️"):
+            edited_transcript = st.text_area(
+                "Ctrl + Enter (x2) to apply transcript",
+                value=st.session_state.transcript,
+                on_change=update_transcript,
+                key="transcript_editor",
+            )
+            st.session_state.transcript = edited_transcript
 
 
 editor_voice_input()
