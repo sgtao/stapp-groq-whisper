a9fdc31acc8bf4a012d42bf80107e5a793c1f0aa
Shogo Ogami <sg.tao.so@gmail.com>
2024-07-28 20:47:02 +0900

    Add:update editor_voice_input page for audio input on modal panel

--- src/pages/12_editor_voice_input.py
+++ src/pages/12_editor_voice_input.py
@@ -22,24 +22,14 @@ def update_transcript():
     st.session_state.transcript_shown = st.session_state.transcript
 
 
+@st.dialog("Transprit from Microphone Record")
 def editor_voice_input():
-    st.title("Editing Voice Input App")
-    st.markdown("Editing and correcting a voice input.")
-
-    # セッション状態の初期化
-    if "transcript" not in st.session_state:
-        st.session_state.transcript = ""
-    if "transcript_shown" not in st.session_state:
-        st.session_state.transcript_shown = ""
-
-    sidebar_key_and_model()
     output_language = language_selector()
     prompt = f"Specify context or spelling in {output_language.language}."
     lang = output_language.lang_code
     st.markdown(f"Transcribe Prompt: **{prompt}**")
 
     # マイク録音
-    st.subheader("Microphone-Record")
     st.session_state.wav_audio_data = st_audiorec()
 
     if st.session_state.wav_audio_data is not None:
@@ -51,22 +41,49 @@ def editor_voice_input():
                 transcript = transcribe_audio(wave_data, prompt, lang)
                 if transcript != "":
                     st.session_state.transcript = transcript
-                    update_transcript()
+            st.write(st.session_state.transcript)
 
-        # show transcript
-        if st.session_state.transcript_shown != "":
-            shown_transcript = f"""{st.session_state.transcript_shown}"""
-            st.code(shown_transcript, language="python")
-
-        # edit transcript
-        with st.expander(label="Edit transcript", icon="🖌️"):
-            edited_transcript = st.text_area(
-                "Ctrl + Enter (x2) to apply transcript",
-                value=st.session_state.transcript,
-                on_change=update_transcript,
-                key="transcript_editor",
-            )
-            st.session_state.transcript = edited_transcript
+        if st.button("Apply to Transcript"):
+            update_transcript()
+            st.rerun()
 
 
-editor_voice_input()
+def edit_transcript():
+    # edit transcript
+    with st.expander(label="Edit transcript", icon="🖌️"):
+        edited_transcript = st.text_area(
+            "Ctrl + Enter (x2) to apply transcript",
+            value=st.session_state.transcript,
+            on_change=update_transcript,
+            key="transcript_editor",
+        )
+        st.session_state.transcript = edited_transcript
+        if st.button("Apply to Transcript"):
+            update_transcript()
+            st.write("click twice to apply to transcript.")
+
+
+# main procedure
+st.title("Editing Voice Input App")
+st.markdown("Editing and correcting a voice input.")
+
+# セッション状態の初期化
+if "transcript" not in st.session_state:
+    st.session_state.transcript = ""
+if "transcript_shown" not in st.session_state:
+    st.session_state.transcript_shown = ""
+
+sidebar_key_and_model()
+
+st.subheader("Microphone-Record")
+if st.button("Record audio?"):
+    editor_voice_input()
+
+# show transcript
+st.subheader("Transcript:")
+if st.session_state.transcript_shown != "":
+    shown_transcript = f"""{st.session_state.transcript_shown}"""
+    st.code(shown_transcript, language="python")
+
+# modify transcript
+edit_transcript()
